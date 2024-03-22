b5b308ca83e5962a4cd38f36923bdefc2e9d539f
Yunjidev <florian.vancamp@gmail.com>
2024-03-22 23:10:36 +0100

    reclone projet fini chronodaowiki

---
+++ src/components/common/nav/Nav.tsx
@@ -0,0 +1,21 @@
+import CTAButton from "../Buttons/CTA-Button/CTAButton";
+import "./Nav.scss";
+import SearchBar from "../SearchBar/Searchbar";
+export default function Nav() {
+  return (
+    <nav>
+      <div className="nav__wrapper">
+        <a href="/" className="nav__link">
+          <img
+            src={`${import.meta.env.CDN_URL}/images/assets/icons/dao.png`}
+            alt="Logo de la guilde ChronoDAO, représente un dragon stylisé bleu dans un cercle rappelant de manière très simplifiée une pocketwatch du jeu Bigtime"
+            className="nav__logo"
+          />
+          <h3 className="nav__title">ChronoDAO Wiki</h3>
+        </a>
+        <SearchBar />
+        <CTAButton text={"Rejoignez nous"} link={"https://discord.gg/chronodao"} />
+      </div>
+    </nav>
+  );
+}
